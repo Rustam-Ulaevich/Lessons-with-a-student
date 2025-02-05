3faffa6563d70cec821d2de5cdce6212a7a1ce8c
Rustam-Ulaevich <praporglamur@gmail.com>
2025-02-05 13:33:28 +0500

    lesson 3

---
+++ js/script3.js
@@ -0,0 +1,100 @@
+// Строковое преобразование
+// let value = undefined
+// console.log(typeof value)
+// value = String(value)
+// console.log(typeof value)
+// console.log(value)
+
+// Численное преобразование
+// console.log('6' / '2');
+// let a = 'Regina'
+// console.log(a);
+// console.log(typeof a);
+// a = Number(a)       строку в число  NaN
+// console.log(a);
+// console.log(typeof a);
+// let a = false
+// console.log(a);
+// console.log(typeof a);
+// a = Number(a)        //boolean в число
+// console.log(a);
+// console.log(typeof a);
+// let a = null
+// console.log(a);
+// console.log(typeof a);
+// a = Number(a)        //null в число 0
+// console.log(a);
+// console.log(typeof a);
+// let a = undefined
+// console.log(a);
+// console.log(typeof a);
+// a = Number(a)        //undefined в число NaN
+// console.log(a);
+// console.log(typeof a);
+// let a = Symbol('id')
+// console.log(a);
+// console.log(typeof a);
+// a = Number(a)        //Symbol в число TypeError!!!
+// console.log(a);
+// console.log(typeof a);
+// let a = {}
+// console.log(a);
+// console.log(typeof a);
+// a = Number(a)        //Object в число NaN
+// console.log(a);
+// console.log(typeof a);
+// let a = 6835531
+// console.log(a);
+// console.log(typeof a);
+// a = Number(a)        //bigInt в число 6135153
+// console.log(a);
+// console.log(typeof a);
+
+
+// Логическое преобразование
+
+// let money = 2
+// console.log(money);
+// console.log(typeof money);
+// money = Boolean(money)     //Число в boolean
+// console.log(money);
+// console.log(typeof money);
+
+// let money = ''
+// console.log(money);
+// console.log(typeof money);
+// money = Boolean(money)     //Строка в boolean
+// console.log(money);
+// console.log(typeof money);
+
+// let money = null
+// console.log(money);
+// console.log(typeof money);
+// money = Boolean(money)     //null в boolean  false
+// console.log(money);
+// console.log(typeof money);
+
+// let money = NaN
+// console.log(money);
+// console.log(typeof money);
+// money = Boolean(money)     //NaN в boolean  false
+// console.log(money);
+// console.log(typeof money);
+
+// let money = -Infinity
+// console.log(money);
+// console.log(typeof money);
+// money = Boolean(money)     //Infinity в boolean  true
+// console.log(money);
+// console.log(typeof money);
+
+let money = Symbol('id')
+console.log(money);
+console.log(typeof money);
+money = Boolean(money)     //Symbol в boolean  true
+console.log(money);
+console.log(typeof money);
+
+
+
+
