daf102118a02722aa24c9ab03f4c454d97b3c08e
Rustam-Ulaevich <praporglamur@gmail.com>
2025-02-25 14:02:35 +0500

    lesson11

--- js/script10.js
+++ js/script10.js
@@ -1,22 +1,22 @@
 
 
-function ageFn(age) {
-    switch (true) {
-    case age<1:{
-        return console.log('Молодой');
-    }       
-    case ( age>=18 && age<=65 ):{
-        return console.log('Взрослый!');
-    }
-    case (age > 65) :{
-        return console.log('Пенсионер');
-    }   
-    default:
-        return console.log('нет данных'); 
-}
-}
-ageFn(9)
-ageFn(25)
-ageFn(175)
-ageFn(' ')
+// function ageFn(age) {
+//     switch (true) {
+//     case age<1:{
+//         return console.log('Молодой');
+//     }       
+//     case ( age>=18 && age<=65 ):{
+//         return console.log('Взрослый!');
+//     }
+//     case (age > 65) :{
+//         return console.log('Пенсионер');
+//     }   
+//     default:
+//         return console.log('нет данных'); 
+// }
+// }
+// ageFn(9)
+// ageFn(25)
+// ageFn(175)
+// ageFn(' ')
 
---
+++ js/script11.js
@@ -0,0 +1,32 @@
+// ФУНКЦИИ
+// let result
+
+// function age(parametr) {
+//     let result = parametr/2
+//     console.log(result+1);
+//     return result
+// }
+
+// console.log(age(56))
+// console.log(result);
+
+//Параметры по умолчанию (дефолтный)
+
+// function str(b, a = 'hello' ) {
+//     return console.log(a + " " + b)
+// }
+
+// str('Рустам')
+
+// let sum = (a, b) =>  a + b
+
+// console.log(sum(2, 3));
+
+function checkAge2(age){age > 18 ? true : confirm('Родители разрешили?')}
+
+console.log(checkAge2(25));
+
+
+
+
+
